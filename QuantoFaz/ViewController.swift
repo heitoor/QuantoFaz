379141b11e776c36357eee2b98ea47d6d1d337f9
Heitor Augusto Pereira <HeitorPereira@macbook-pro-de-heitor.local>
2016-02-03 20:23:46 -0200

    Recebe os valores e calcula.
    Bugs: Se não inserir valor, ele trava.

--- QuantoFaz/ViewController.swift
+++ QuantoFaz/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var precoEtanol: UITextField!
+    @IBOutlet weak var precoGasolina: UITextField!
+    @IBOutlet weak var consumoEtanol: UITextField!
+    @IBOutlet weak var consumoGasolina: UITextField!
+    @IBOutlet weak var displayFinal: UILabel!
+    
+    
+    
+    @IBAction func calcular(sender: UIButton) {
+        
+        let pEtanol = Float(precoEtanol.text!)
+        let pGasolina = Float(precoGasolina.text!)
+        let cEtanol = Float(consumoEtanol.text!)
+        let cGasolina = Float(consumoGasolina.text!)
+        
+        let gastoEtanol = pEtanol! / cEtanol!
+        let gastoGasolina = pGasolina! / cGasolina!
+        
+        if gastoEtanol > gastoGasolina {
+            displayFinal.text = "O seu carro gasta R$ \(gastoGasolina) por KM com gasolina. \n E R$ \(gastoEtanol) com etanol. Portanto, utilize Gasolina."
+        } else {
+            displayFinal.text = "O seu carro gasta R$ \(gastoEtanol) por KM com etanol. \n E R$ \(gastoGasolina) com gasolina. Portanto, utilize Etanol."
+        }
+        self.consumoGasolina.resignFirstResponder()
+        self.consumoEtanol.resignFirstResponder()
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
